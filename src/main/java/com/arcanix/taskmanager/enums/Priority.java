8eba3ce49550c51cd4f624a4e35e02f4d327e9d7
Alexander <oliveir4.4lexander@gmail.com>
2026-06-05 10:58:30 -0300

    Criadas entity e enums

---
+++ src/main/java/com/arcanix/taskmanager/enums/Priority.java
@@ -0,0 +1,7 @@
+package com.arcanix.taskmanager.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/arcanix/taskmanager/enums/Status.java
@@ -0,0 +1,7 @@
+package com.arcanix.taskmanager.enums;
+
+public enum Status {
+    CREATED,
+    IN_PROGRESS,
+    DONE
+}
