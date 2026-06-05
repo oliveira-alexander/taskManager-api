09281518766ee6625f9f334e6bb0ddee71fc3316
Alexander <oliveir4.4lexander@gmail.com>
2026-06-05 11:30:21 -0300

    Adicionado Mapper

--- src/main/java/com/arcanix/taskmanager/dtos/CreateTaskDTO.java
+++ src/main/java/com/arcanix/taskmanager/dtos/CreateTaskDTO.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 
 public record CreateTaskDTO(String title,
                             String description,
-                            LocalDateTime createdAt,
                             LocalDateTime deadline,
                             Priority priority,
                             Status status) {
---
+++ src/main/java/com/arcanix/taskmanager/mapper/TaskMapper.java
@@ -0,0 +1,40 @@
+package com.arcanix.taskmanager.mapper;
+
+import com.arcanix.taskmanager.dtos.CreateTaskDTO;
+import com.arcanix.taskmanager.dtos.ResponseTaskDTO;
+import com.arcanix.taskmanager.dtos.UpdateTaskDTO;
+import com.arcanix.taskmanager.entities.Task;
+
+import java.time.LocalDateTime;
+
+public class TaskMapper {
+    public static Task createDtoToTask(CreateTaskDTO dto){
+        return new Task(null,
+                        dto.title(),
+                        dto.description(),
+                        LocalDateTime.now(),
+                        dto.deadline(),
+                        dto.priority(),
+                        dto.status());
+    }
+
+    public static Task updateDtoToTask(UpdateTaskDTO dto){
+        return new Task(dto.id(),
+                dto.title(),
+                dto.description(),
+                dto.createdAt(),
+                dto.deadline(),
+                dto.priority(),
+                dto.status());
+    }
+
+    public static ResponseTaskDTO taskToResponseDTO(Task task){
+        return new ResponseTaskDTO(task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getCreatedAt(),
+                task.getDeadline(),
+                task.getPriority(),
+                task.getStatus());
+    }
+}
